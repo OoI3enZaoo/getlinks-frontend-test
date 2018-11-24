1fff02279596456546decde319f04611e544e62d
OoI3enZaoO <Teerapath-Ben@hotmail.com>
2018-11-24 16:49:17 +0700

    update: change restify to express

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-const restify = require('restify')
-const server = restify.createServer()
+const path = require('path')
+const express = require('express')
+const server = express()
 
 server.use(compression())
 
 app.prepare().then(() => {
+  server.get('/sw.js', (req, res) => {
+    app.serveStatic(req, res, path.resolve('./static/sw.js'))
+  })
+  server.get('/robots.txt', (req, res) => {
+    res.sendFile(path.join(__dirname, '/static', 'robots.txt'))
+  })
   server.get('*', (req, res) => {
     return handle(req, res)
-  })
+  })  
   server.listen(port, () => console.log('server is running on port: ', port))
 })
