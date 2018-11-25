9a7f985ee7a60554aa50f38c4d5c2e1e57e353b0
OoI3enZaoO <Teerapath-Ben@hotmail.com>
2018-11-25 15:09:47 +0700

    update: add "yarn deploy:static" command for export project to static with service worker

---
+++ out/sw.js
@@ -0,0 +1,128 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "325a006155dfd920a88e169f53ebb03d"
+  },
+  {
+    "url": "_next/static/6Ce~NI_7CftUfCy3f~91H/pages/_app.js",
+    "revision": "2f5d5769a423ae766adba91493401b27"
+  },
+  {
+    "url": "_next/static/6Ce~NI_7CftUfCy3f~91H/pages/_error.js",
+    "revision": "9df55d5895003a0d5435e873323a095c"
+  },
+  {
+    "url": "_next/static/6Ce~NI_7CftUfCy3f~91H/pages/index.js",
+    "revision": "3ce5e56fa5d8585640bb37f974f0f25c"
+  },
+  {
+    "url": "_next/static/chunks/0.js",
+    "revision": "a72c707134870212974b68a7372c06f4"
+  },
+  {
+    "url": "_next/static/chunks/commons.cb73b43cd461ffe72e80.js",
+    "revision": "f407935a0022b5488181fae7e395dbba"
+  },
+  {
+    "url": "_next/static/cUzrW~w8ONL48bYuBAIvB/pages/_app.js",
+    "revision": "caec3e9789daff6bde0f648384cd0387"
+  },
+  {
+    "url": "_next/static/cUzrW~w8ONL48bYuBAIvB/pages/_error.js",
+    "revision": "380cd0e8f2a4f28be6de31533d2384b3"
+  },
+  {
+    "url": "_next/static/cUzrW~w8ONL48bYuBAIvB/pages/index.js",
+    "revision": "cb9a68f51998f1aeb0943c899402d677"
+  },
+  {
+    "url": "_next/static/CxZ5e0mMfYLAhvEFk1lPb/pages/_app.js",
+    "revision": "caec3e9789daff6bde0f648384cd0387"
+  },
+  {
+    "url": "_next/static/CxZ5e0mMfYLAhvEFk1lPb/pages/_error.js",
+    "revision": "380cd0e8f2a4f28be6de31533d2384b3"
+  },
+  {
+    "url": "_next/static/CxZ5e0mMfYLAhvEFk1lPb/pages/index.js",
+    "revision": "cb9a68f51998f1aeb0943c899402d677"
+  },
+  {
+    "url": "_next/static/development/dll/dll_86d7de3159d32d38a4d2.js",
+    "revision": "59f542a63a3e7ef1337a9da1be23de19"
+  },
+  {
+    "url": "_next/static/development/pages/_app.js",
+    "revision": "f9a347b2c683f9ad4609523f68414730"
+  },
+  {
+    "url": "_next/static/development/pages/_error.js",
+    "revision": "c485913097b18e80233945d052dea93e"
+  },
+  {
+    "url": "_next/static/evjJ3GOitspXG0DgXjdnU/pages/_app.js",
+    "revision": "caec3e9789daff6bde0f648384cd0387"
+  },
+  {
+    "url": "_next/static/evjJ3GOitspXG0DgXjdnU/pages/_error.js",
+    "revision": "380cd0e8f2a4f28be6de31533d2384b3"
+  },
+  {
+    "url": "_next/static/evjJ3GOitspXG0DgXjdnU/pages/index.js",
+    "revision": "cb9a68f51998f1aeb0943c899402d677"
+  },
+  {
+    "url": "_next/static/Ny_qSsff4QFAzGN6Nt1yp/pages/_app.js",
+    "revision": "2f5d5769a423ae766adba91493401b27"
+  },
+  {
+    "url": "_next/static/Ny_qSsff4QFAzGN6Nt1yp/pages/_error.js",
+    "revision": "9df55d5895003a0d5435e873323a095c"
+  },
+  {
+    "url": "_next/static/Ny_qSsff4QFAzGN6Nt1yp/pages/index.js",
+    "revision": "3ce5e56fa5d8585640bb37f974f0f25c"
+  },
+  {
+    "url": "_next/static/runtime/main-74c9050b2bb1bdfe8c29.js",
+    "revision": "349b0c0eabfcd9e216566fb1cad4cbcf"
+  },
+  {
+    "url": "_next/static/runtime/main.js",
+    "revision": "cc93c118edd8de14d84b01fb0dfda472"
+  },
+  {
+    "url": "_next/static/runtime/webpack-42652fa8b82c329c0559.js",
+    "revision": "2678b70926bdf0f2081ca40f4e674090"
+  },
+  {
+    "url": "_next/static/runtime/webpack.js",
+    "revision": "c88b235de177028840dc0d103f59bf56"
+  },
+  {
+    "url": "_next/static/webpack/static/runtime/main.js.e26c648053391a86e85c.hot-update.js",
+    "revision": "af4252ea77de5578699ac440fe912507"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
