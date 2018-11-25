d14967e900881bb3aa729694f6556d6306c57002
OoI3enZaoO <Teerapath-Ben@hotmail.com>
2018-11-25 11:22:03 +0700

    update: add sitemap & change url sitemap in robots.txt

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 const path = require('path')
 const express = require('express')
+const sm = require('sitemap')
 const server = express()
 
 server.use(compression())
+
+const sitemap = sm.createSitemap({
+  hostname: 'https://getlinks-testing-frontend.herokuapp.com',
+  cacheTime: 600000, // 600 sec - cache purge period
+  urls: [
+    { url: '/', changefreq: 'daily', priority: 1 },
+  ],
+})
 
 app.prepare().then(() => {
   server.get('/sw.js', (req, res) => {
@@ -16,6 +25,16 @@ app.prepare().then(() => {
   })
   server.get('/robots.txt', (req, res) => {
     res.sendFile(path.join(__dirname, '/static', 'robots.txt'))
+  })
+  server.get('/sitemap.xml', (req, res) => {
+    sitemap.toXML((err, xml) => {
+      if (err) {
+        res.status(500).end()
+        return
+      }
+      res.header('Content-Type', 'application/xml')
+      res.send(xml)
+    })
   })
   server.get('*', (req, res) => {
     return handle(req, res)
